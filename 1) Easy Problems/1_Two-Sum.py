efd4d667781433b61451a6755e840689648be639
John Dale <116762794+JohnDale02@users.noreply.github.com>
2023-01-10 16:31:02 -0500

    Update 1_Two-Sum.py

--- 1) Easy Problems/1_Two-Sum.py
+++ 1) Easy Problems/1_Two-Sum.py
@@ -12,3 +12,26 @@ class Solution:
                 else:
                     continue
             slide += 1
+
+            '''         # Double Pass Hash Map Solution
+    class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]] 
+    '''
+
+    '''                 # Single Pass Hash Map Solution
+    class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
+    '''
